70cb27e64dee08e1095aedf988311b80f8aafeb8
AfonsoSeguro <56278710+AfonsoSeguro@users.noreply.github.com>
2025-04-01 11:33:31 +0100

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -15,7 +15,7 @@ function inic(){
 
 function render(){		
 	
-		if(fps % 10 == 0){
+		if(fps % 8 == 0){
 			cobra.mover();
 			if(cobra.cabeca.x == x && cobra.cabeca.y == y){
 				maca = new Maca(cobra.corpo);
